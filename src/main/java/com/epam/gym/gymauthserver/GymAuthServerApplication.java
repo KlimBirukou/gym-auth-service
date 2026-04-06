10b451c51b1c2780d1af24d8fc63b9caead93890
klimb <manulc0rparation@gmail.com>
2026-04-06 11:18:48 +0400

    init auth server

---
+++ src/main/java/com/epam/gym/gymauthserver/GymAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.epam.gym.gymauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymAuthServerApplication.class, args);
+    }
+
+}
